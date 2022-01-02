0d301effc028973ebdfc64d857bdc212b44769cf
Arka Saha <arka.saha107@gmail.com>
2022-01-01 20:15:58 -0500

    init

---
+++ Day 1/Day1-Project- band Name Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator")
+city = input("Enter the City you were Born in?\n")
+pet = input("Enter the Name of Your Pet?\n")
+print("Your Band Name is -"+" "+city+" "+pet)
